2b7907dddbc6c0958c0a529f6d25caadf44b2c0c
gabriellnuness <40604481+gabriellnuness@users.noreply.github.com>
2021-11-26 13:47:35 -0300

    Function to save all open figures

---
+++ save_open_fig.m
@@ -0,0 +1,45 @@
+function save_open_fig(path, size, ext)
+% This function saves all the opened figures at the moment in MATLAB and saves in the indicated folder, inside a 'plots' folder.
+% The name of each file is the name of the figure define in: figure('name','...')
+% path: 'D:\Users\...'
+% size: [width  height]
+% ext:    1 - pdf
+%         2 - eps
+%         3 - png
+%         4 - ALL 
+%
+% save_open_fig(path, size, format)
+
+    paperunits='centimeters';
+    plot_path = [path,'\plots']
+    figHandles = findall(0,'Type','figure'); 
+    initial_path = pwd;
+
+    cd(path)
+
+    if ~exist('plots', 'dir')
+        mkdir('plots')
+    end
+    
+    cd(plot_path)
+         
+    for i = 1:numel(figHandles)
+        set(figHandles(i),'paperunits', paperunits, 'paperposition', [0 0 size]);
+        set(figHandles(i), 'PaperSize', size);
+        
+        if ext == 1
+            print(figHandles(i), [figHandles(i).Name,'.pdf'],'-dpdf')
+        elseif ext == 2
+            print(figHandles(i), [figHandles(i).Name,'.eps'],'-depsc')
+        elseif ext == 3
+            print(figHandles(i), [figHandles(i).Name,'.png'],'-dpng')
+        elseif ext == 4
+            print(figHandles(i), [figHandles(i).Name,'.pdf'],'-dpdf')
+            print(figHandles(i), [figHandles(i).Name,'.eps'],'-depsc')
+            print(figHandles(i), [figHandles(i).Name,'.png'],'-dpng')
+        end
+    end
+
+    cd(initial_path)
+    fprintf('Done saving the figures!\n')
+end
